44e6a7f20f8c6391ee50f5f523cdd0b9db4c25eb
Tim Hudson <tim.hudson15@gmail.com>
2019-11-26 05:38:49 -0800

    Remove vscode settings in favor of gist

--- vscode/install.sh
+++
@@ -1,2 +0,0 @@
-# VSCode's settings.json file needs to live in a different folder from the user root
-ln -sf "${HOME}/.vscode.settings.json" "${HOME}/Library/Application Support/VSCodium/User/settings.json"
