a18eff44cd733fac5570045ee66376adaf4abcb9
Tim Hudson <tim.hudson15@gmail.com>
2019-11-27 14:04:05 -0800

    Add goto function

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,6 +1,10 @@
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
+# Load custom functions
+fpath=($ZSH/functions $fpath)
+autoload -U $ZSH/functions/*(:t)
+
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
