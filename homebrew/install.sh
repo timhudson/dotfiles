01f7c975136bc2c2b11b36dc74e1f1f37cfb7b3f
Tim Hudson <tim.hudson@shipveho.com>
2023-03-23 11:07:48 -0700

    Add GitHub Copilot CLI

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 # Homebrew
 #
@@ -24,4 +24,20 @@ then
   brew bundle
 fi
 
-exit 0
+NODE_VERSION=18
+
+fnm install $NODE_VERSION
+fnm default $NODE_VERSION
+fnm use default
+
+# Install global NPM packages
+packages=(
+  @githubnext/github-copilot-cli
+)
+
+npm install -g "${packages[@]}"
+
+# Set up GitHub Copilot CLI aliases
+# Storing these in a file in the dotfiles repo so they can be versioned.
+# We'll update the aliases upon install, but may need to commit changes if necessary
+echo "$(github-copilot-cli alias -- "$0")" > copilot-aliases.zsh
